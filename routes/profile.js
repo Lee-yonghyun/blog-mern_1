871d92797c2a8f1bfdeaf6ee645a7b044054136a
이용현 <dydwk231@naver.com>
2020-09-10 22:26:11 +0900

    add profile,user model,routes

---
+++ routes/profile.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const profileModel = require('../models/profile')
+
+
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -5,12 +5,19 @@ const bodyParser = require('body-parser')
 const dotEnv = require('dotenv')
 dotEnv.config()
 require('./config/database')
+const userRoutes = require('./routes/user')
+const profileRoutes = require('./routes/profile')
 
 
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
+
+
+
+app.use('/user',userRoutes)
+app.use('/profile', profileRoutes)
 
 
 
