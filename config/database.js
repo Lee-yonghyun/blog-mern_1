bd33c78e5af5a793142e300b706bc437168e6750
이용현 <dydwk231@naver.com>
2020-09-10 22:39:34 +0900

    add user model schema & signup data

--- config/database.js
+++ config/database.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 const dboptions ={
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: true
+    useFindAndModify: true,
+    useCreateIndex: true
+
 }
 
 mongoose
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,31 @@ const router = express.Router()
 const userModel = require('../models/user')
 
 
+router.post('/registor',(req,res) => {
+
+    const {name, email, password} = req.body
+
+    const newUser = new userModel({
+        name,
+        email,
+        password
+    })
+
+    newUser
+        .save()
+        .then(user => {
+            res.json({
+                message:'saved data',
+                userInfo: user
+            })
+        })
+        .catch(err => {
+            res.json({
+                message:err.message
+            })
+        })
+})
+
 
 
 module.exports = router
