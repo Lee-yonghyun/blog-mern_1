fe721b71642f3a8b1e8a77b7ff284e42e5d4287a
이용현 <dydwk231@naver.com>
2020-09-19 16:00:40 +0900

    add profile register

--- models/profile.js
+++ models/profile.js
@@ -1,7 +1,49 @@
 const mongoose = require('mongoose')
 
 
-const profileSchema = new mongoose.Schema()
+const profileSchema = new mongoose.Schema(
+    {
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"user",
+            required:true
+        },
+        introduce : {
+            type : String,
+            required : true,
+            max : 40
+        },
+        company : {
+            type : String
+        },
+        website : {
+            type : String
+        },
+        location : {
+            type : String,
+        },
+        status : {
+            type : String,
+            required : true
+        },
+        skills : {
+            type : [String],
+            required : true
+        },
+        gender : {
+            type : String,
+            default : "male"
+        },
+        githubusername : {
+            type : String
+        },
+        experience : [],
+        education : []
+    },
+    {
+        timestamps: true
+    }
+);
 
 
 module.exports = mongoose.model('profile',profileSchema)
--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,60 @@
 const express = require('express')
 const router = express.Router()
 const profileModel = require('../models/profile')
+const passport = require('passport')
+const checkAuth = passport.authenticate("jwt",{session: false})
 
+
+//프로필 등록
+
+router.post('/registor',checkAuth,(req,res) => {
+
+    const profileFields ={};
+
+    profileFields.user = req.user.id //checkAuth에서 받아온 id (payload에 담긴)
+
+    if(req.body.introduce) profileFields.introduce = req.body.introduce;
+    if(req.body.company) profileFields.company = req.body.company;
+    if(req.body.website) profileFields.website = req.body.website;
+    if(req.body.location) profileFields.location = req.body.location;
+    if(req.body.status) profileFields.status = req.body.status;
+    if(req.body.gender) profileFields.gender = req.body.gender;
+    if(req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+
+    if(typeof req.body.skills !== "undefined" || req.body.skills.length !==0 ){
+        profileFields.skills = req.body.skills.split(",");
+    }
+
+    profileModel
+        .findOne({user:req.user.id})
+        .then(profile => {
+            //profile이 있다면?
+            if(profile){
+                return res.status(200).json({
+                    message: "profile already exists, please update profile"
+                })
+            }
+            //profile이 x
+            else {
+                new profileModel(profileFields)
+                    .save()
+                    .then(profile => res.status(200).json(profile))
+                    .catch(err => {
+                        res.status(400).json({
+                            message:err.message
+                        })
+                    })
+            }
+
+        })
+        .catch(err => {
+            res.json({
+                message:err.message
+            })
+        })
+
+
+})
 
 
 
